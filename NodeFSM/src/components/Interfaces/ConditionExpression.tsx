92642c4d7b42d43e7674a434a4cca4009f1d8544
AathreyaDA <144434793+AathreyaDA@users.noreply.github.com>
2024-11-22 12:24:41 +0530

    Delete NodeFSM directory

--- NodeFSM/src/components/Interfaces/ConditionExpression.tsx
+++
@@ -1,8 +0,0 @@
-enum ConditionExpression{
-    EQ = "equals",
-    NE = "not equals",
-    GT = "greater than",
-    LT = "lesser than"
-}
-
-export default ConditionExpression;
--- NodeFSM/src/components/Interfaces/MenuOptions.tsx
+++
@@ -1,7 +0,0 @@
-interface MenuOptions {
-    wrapperElement: HTMLElement;
-    position: { x: number; y: number };
-    items: string[];
-  }
-
-export default MenuOptions;
